682c2ac4ab118d7466e3dc0dd3bfd78076bf3795
Mpho077 <69971504+Mpho077@users.noreply.github.com>
2026-03-09 20:20:15 +0200

    Delete shopify_odoo_sync directory

--- shopify_odoo_sync/models/product_product.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    shopify_variant_id = fields.Char(string="Shopify Variant ID", index=True, copy=False)
-    shopify_inventory_item_id = fields.Char(string="Shopify Inventory Item ID", copy=False)
