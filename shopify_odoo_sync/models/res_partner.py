f1127aa12f8a71bb105158749dd38267ccacc8e0
Mpho077 <69971504+Mpho077@users.noreply.github.com>
2026-03-09 19:42:42 +0200

    Delete shopify_odoo_sync directory

--- shopify_odoo_sync/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    shopify_customer_id = fields.Char(string="Shopify Customer ID", index=True, copy=False)
-    shopify_backend_id = fields.Many2one('shopify.backend', string="Shopify Store", copy=False)
